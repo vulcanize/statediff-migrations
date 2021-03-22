c9898e05fe99bd2f57df81c89a10b98daeb20a2e
ramil <ramilexe@gmail.com>
2021-03-22 12:31:22 +0300

    change storage_cids_id_seq sequence to bigint

--- db/migrations/00020_state_cids_bigint.sql
+++ db/migrations/00020_state_cids_bigint.sql
@@ -1,5 +1,7 @@
 -- +goose Up
 ALTER SEQUENCE eth.state_cids_id_seq as bigint NO MAXVALUE;
+ALTER SEQUENCE eth.storage_cids_id_seq as bigint NO MAXVALUE;
 
 -- +goose Down
+ALTER SEQUENCE eth.storage_cids_id_seq as int NO MAXVALUE;
 ALTER SEQUENCE eth.state_cids_id_seq as int NO MAXVALUE;
