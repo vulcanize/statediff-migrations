f66d5e5f07c689d1e1dd901faa6b2dbd2dcaae0f
Arijit Das <arijitad.in@gmail.com>
2021-08-19 18:27:30 +0530

    Remove `mh_key` and `cid` from log table.

--- db/migrations/00025_create_eth_log_cids_table.sql
+++ db/migrations/00025_create_eth_log_cids_table.sql
@@ -3,9 +3,7 @@ CREATE TABLE eth.log_cids (
     id                  SERIAL PRIMARY KEY,
     receipt_id          INTEGER NOT NULL REFERENCES eth.receipt_cids (id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
     address             VARCHAR(66),
-    cid                 TEXT NOT NULL,
     log_data            BYTEA,
-    mh_key              TEXT NOT NULL REFERENCES public.blocks (key) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
     index               INTEGER NOT NULL,
     topic0              VARCHAR(66),
     topic1              VARCHAR(66),
@@ -23,10 +21,6 @@ DROP COLUMN log_contracts,
 ADD COLUMN log_root VARCHAR(66);
 
 CREATE INDEX log_rct_id_index ON eth.log_cids USING btree (receipt_id);
-
-CREATE INDEX log_mh_index ON eth.log_cids USING btree (mh_key);
-
-CREATE INDEX log_cid_index ON eth.log_cids USING btree (cid);
 
 --
 -- Name: log_topic0_index; Type: INDEX; Schema: eth; Owner: -
