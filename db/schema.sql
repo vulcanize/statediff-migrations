1cfee7d9a2dc60bbc77406da328f820e43b85dc2
Arijit Das <arijitad.in@gmail.com>
2021-09-20 17:01:50 +0530

    Update the schema diff.

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.12
--- Dumped by pg_dump version 13.3
+-- Dumped from database version 14beta3
+-- Dumped by pg_dump version 13.4 (Ubuntu 13.4-1.pgdg20.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
