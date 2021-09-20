8444c53208c2566c16a4ebe82d4a06840425ee6c
Arijit Das <arijitad.in@gmail.com>
2021-09-20 16:11:28 +0530

    Drop unused functions.

--- db/migrations/00027_rename_cid_and_mh_key_in_eth_receipt_cids.sql
+++ db/migrations/00027_rename_cid_and_mh_key_in_eth_receipt_cids.sql
@@ -5,8 +5,14 @@ RENAME COLUMN cid TO leaf_cid;
 ALTER TABLE eth.receipt_cids
 RENAME COLUMN mh_key TO leaf_mh_key;
 
-ALTER INDEX eth.rct_mh_index RENAME TO rct_leaf_cid_index;
-ALTER INDEX eth.rct_cid_index RENAME TO rct_leaf_mh_key_index;
+ALTER INDEX eth.rct_mh_index
+RENAME TO rct_leaf_cid_index;
+
+ALTER INDEX eth.rct_cid_index
+RENAME TO rct_leaf_mh_key_index;
+
+DROP FUNCTION header_weight;
+DROP FUNCTION canonical_header;
 
 -- +goose Down
 DROP INDEX eth.rct_leaf_cid_index;
